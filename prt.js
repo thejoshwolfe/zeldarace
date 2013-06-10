d65daa366860a0f637ada9c37c8c66cfd79cb921
Andrew Kelley <superjoe30@gmail.com>
2013-06-10 03:24:37 -0400

    add some useless info

--- prt.js
+++ prt.js
@@ -40,9 +40,49 @@ function saveState() {
 })();
 
 var startAudioBtn = document.createElement("button");
-startAudioBtn.innerText = "Start!";
+startAudioBtn.innerText = "Mario Cart";
 var happyFunTimeAudio = new Audio("mario-kart.ogg");
 startAudioBtn.addEventListener("click", function() {
   happyFunTimeAudio.play();
 });
 document.body.appendChild(startAudioBtn);
+
+
+var checkpoints = [
+  {
+    name: "Deku Tree",
+    desc: "Pause immediately after the cutscene after...",
+  },
+  {
+    name: "Dodongo's Cavern",
+    desc: "Pause at some point...",
+  },
+  {
+    name: "Adult Link",
+    desc: "Pause",
+  },
+  {
+    name: "Forest Temple",
+    desc: "Pause",
+  },
+  {
+    name: "Fire Temple",
+    desc: "Pause",
+  },
+  {
+    name: "Water Temple",
+    desc: "Pause",
+  },
+  {
+    name: "Shadow Temple",
+    desc: "Pause",
+  },
+  {
+    name: "Spirit Temple",
+    desc: "Pause",
+  },
+  {
+    name: "Ganon's Castle",
+    desc: "Pause",
+  },
+];
