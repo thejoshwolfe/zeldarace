02150ac83a0f62bd6cc3f3cb9714ddf760e885f2
Josh Wolfe <thejoshwolfe@gmail.com>
2013-06-09 23:47:16 -0700

    persisting state

--- r.js
+++ r.js
@@ -3,20 +3,24 @@ function getElement(id) {
   return document.getElementById(id);
 }
 var people_list = getElement("people_list");
-function addMan(id) {
-  var person = {
-    id: id
-  };
+function addPerson(person) {
+  people.push(person);
   var item = document.createElement("li");
-  item.setAttribute("id", id);
-  item.innerText = id;
+  item.setAttribute("id", person.id);
+  item.innerText = person.id;
   people_list.appendChild(item);
 }
 getElement("add_person_button").addEventListener("click", function() {
-  var text = getElement("add_person_text").value;
-  addMan(text);
+  var id = getElement("add_person_text").value;
+  addPerson({id: id});
+  saveState();
 });
 function saveState() {
-  var people = [];
-  localStorage.asdf = "asdf";
+  localStorage.people = JSON.stringify(people);
 }
+(function loadState() {
+  if (localStorage.people) {
+    var loaded_people = JSON.parse(localStorage.people);
+    loaded_people.forEach(addPerson);
+  }
+})();
