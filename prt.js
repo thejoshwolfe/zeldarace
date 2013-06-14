8ceb6ded159d1f2e711939881f74a0c8b3c19699
Josh Wolfe <thejoshwolfe@gmail.com>
2013-06-13 22:26:35 -0700

    empty table of times

--- prt.js
+++ prt.js
@@ -1,46 +1,46 @@
-var checkpoints = [
-  {
-    name: "Deku Tree",
-    desc: "Pause immediately after the cutscene after...",
-  },
-  {
-    name: "Dodongo's Cavern",
-    desc: "Pause at some point...",
-  },
-  {
-    name: "Adult Link",
-    desc: "Pause",
-  },
-  {
-    name: "Forest Temple",
-    desc: "Pause",
-  },
-  {
-    name: "Fire Temple",
-    desc: "Pause",
-  },
-  {
-    name: "Water Temple",
-    desc: "Pause",
-  },
-  {
-    name: "Shadow Temple",
-    desc: "Pause",
-  },
-  {
-    name: "Spirit Temple",
-    desc: "Pause",
-  },
-  {
-    name: "Ganon's Castle",
-    desc: "Pause",
-  },
-];
 
 window.APP = window.angular.module('main', []).controller('MainCtrl', function($scope) {
   $scope.state = {
     people: [],
     gameState: 'setup',
+    checkpoints: [
+      {
+        name: "Deku Tree",
+        desc: "Pause immediately after the cutscene after...",
+      },
+      {
+        name: "Dodongo's Cavern",
+        desc: "Pause at some point...",
+      },
+      {
+        name: "Adult Link",
+        desc: "Pause",
+      },
+      {
+        name: "Forest Temple",
+        desc: "Pause",
+      },
+      {
+        name: "Fire Temple",
+        desc: "Pause",
+      },
+      {
+        name: "Water Temple",
+        desc: "Pause",
+      },
+      {
+        name: "Shadow Temple",
+        desc: "Pause",
+      },
+      {
+        name: "Spirit Temple",
+        desc: "Pause",
+      },
+      {
+        name: "Ganon's Castle",
+        desc: "Pause",
+      },
+    ],
   };
 
   $scope.addPerson = function() {
