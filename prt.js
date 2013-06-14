0f7e3cee15fcdc632b218a7272061cca78012351
Josh Wolfe <thejoshwolfe@gmail.com>
2013-06-13 21:42:13 -0700

    refreshing entire ui with a render function

--- prt.js
+++ prt.js
@@ -3,38 +3,44 @@ function getElement(id) {
   return document.getElementById(id);
 }
 var people_list = getElement("people_list");
-function addPerson(person) {
-  if (people[person.id]) return;
-  people[person.id] = person;
-  var item = document.createElement("li");
-  item.setAttribute("id", person.id);
-  item.appendChild(document.createTextNode(person.id));
-  var delete_button = document.createElement("button");
-  delete_button.innerText = "Delete";
-  item.appendChild(delete_button);
-  people_list.appendChild(item);
-  delete_button.addEventListener("click", function() {
-    delete people[person.id];
-    people_list.removeChild(item);
-    saveState();
-  });
-}
 getElement("add_person_button").addEventListener("click", function() {
   var id = getElement("add_person_text").value;
-  addPerson({id: id});
+  var person = {id: id};
+  if (people[person.id]) return;
+  people[person.id] = person;
   saveState();
+  render();
 });
 function saveState() {
   localStorage.people = JSON.stringify(people);
 }
 (function loadState() {
   if (localStorage.people) {
-    var loaded_people = JSON.parse(localStorage.people);
-    for (var k in loaded_people) {
-      addPerson(loaded_people[k]);
-    }
+    people = JSON.parse(localStorage.people);
+    render();
   }
 })();
+function render() {
+  people_list.innerHTML = "";
+  for (var id in people) {
+    var person = people[id];
+    makeListItem(person);
+  }
+  function makeListItem(person) {
+    var item = document.createElement("li");
+    item.setAttribute("id", person.id);
+    item.appendChild(document.createTextNode(person.id));
+    var delete_button = document.createElement("button");
+    delete_button.innerText = "Delete";
+    item.appendChild(delete_button);
+    people_list.appendChild(item);
+    delete_button.addEventListener("click", function() {
+      delete people[person.id];
+      people_list.removeChild(item);
+      saveState();
+    });
+  }
+}
 
 var happyFunTimeAudio = new Audio("mario-kart.ogg");
 getElement("mario_cart_button").addEventListener("click", function() {
